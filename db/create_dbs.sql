226b48ad75e949aba681b4b287f4a49db9552859
Valentyn Levytskyi <valevytskyi@icloud.com>
2026-04-27 13:44:21 +0200

    DB: Add user for analytics_api

--- db/create_dbs.sql
+++ db/create_dbs.sql
@@ -7,4 +7,9 @@ GRANT INSERT ON aware_android.* TO 'aware_android_participant'@'%';
 CREATE USER IF NOT EXISTS 'aware_ios_participant'@'%' IDENTIFIED BY 'participantpass';
 GRANT INSERT ON aware_ios.* TO 'aware_ios_participant'@'%';
 
+-- Read-only user restricted to the analytics API container only
+CREATE USER IF NOT EXISTS 'aware_analytics'@'aware_dashboard_api' IDENTIFIED BY 'analyticspass';
+GRANT SELECT ON aware_android.* TO 'aware_analytics'@'aware_dashboard_api';
+GRANT SELECT ON aware_ios.*     TO 'aware_analytics'@'aware_dashboard_api';
+
 FLUSH PRIVILEGES;
