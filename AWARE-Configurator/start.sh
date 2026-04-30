36920b9af36cf883e79cce1dd359bbda641066ab
Valentyn Levytskyi <valevytskyi@icloud.com>
2026-04-30 13:14:20 +0200

    Configurator: run collectstatic at build time to cut startup

--- AWARE-Configurator/start.sh
+++ AWARE-Configurator/start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -eu
 
-python manage.py collectstatic --noinput
-find reactapp/build -maxdepth 1 -type f ! -name 'index.html' -exec cp {} staticfiles/ \;
+# Seed the staticfiles volume from the pre-baked build-time copy (fast path).
+# Falls back to collectstatic only if the baked directory is somehow missing.
+if [ -d /app/staticfiles_baked ] && [ "$(ls -A /app/staticfiles_baked)" ]; then
+    cp -rn /app/staticfiles_baked/. /app/staticfiles/
+else
+    python manage.py collectstatic --noinput
+    find reactapp/build -maxdepth 1 -type f ! -name 'index.html' -exec cp {} staticfiles/ \;
+fi
 
 exec gunicorn aware_light_config_Django.wsgi:application -c util/gunicorn.conf.py
